689f6a5f595f898e14a2d5542879f13857c040cf
Jessica Prevost <jessicaprevost@Jessicas-MacBook-Air-2.local>
2025-07-23 16:42:14 +0200

    Separated cleaning and analysis scripts and added cleaned daily and hourly cleaned data

--- code/02_streamflow_analysis.R
+++ code/02_streamflow_analysis.R
@@ -1,4 +1,4 @@
-### Streamflow data exploration 
+### Streamflow data cleaning and exploration 
 
 # Load packages
 library(tidyverse)
@@ -23,15 +23,16 @@ ddat <- ddat %>%
   mutate(
     across(
       c(`Air Temperature Max`, `Air Temperature Min`, `Relative Humidity Min`),
-      ~ ifelse(. < -50, NA, .)
+      ~ ifelse(. < -50 | . == 0, NA, .)
     ),
-    `Relative Humidity Max` = ifelse(`Relative Humidity Max` < -50 | `Relative Humidity Max` > 1000, NA, `Relative Humidity Max`),
+    `Relative Humidity Max` = ifelse(`Relative Humidity Max` < -50 | `Relative Humidity Max` > 1000 | `Relative Humidity Max` == 0, NA, `Relative Humidity Max`),
     across(
       c(`Soil Moisture 30cm Max`, `Soil Moisture 20cm Max`, `Soil Moisture 10cm Max`,
         `Soil Moisture 30cm Min`, `Soil Moisture 20cm Min`, `Soil Moisture 10cm Min`),
       ~ ifelse(. < 1 | Date < as.Date("2015-01-01"), NA, .)
     )
   )
+## Potentially problematic values: max and min humidity values of 100
 
 hdat <- hdat %>%
   mutate(
@@ -39,10 +40,10 @@ hdat <- hdat %>%
       c(`Air Temperature`),
       ~ ifelse(. < -50, NA, .)
     ),
-    `Relative Humidity` = ifelse(`Relative Humidity` < -50 | `Relative Humidity` > 1000, NA, `Relative Humidity`)
+    `Relative Humidity` = ifelse(`Relative Humidity` < -50 | `Relative Humidity` > 1000 | `Relative Humidity` == 0, NA, `Relative Humidity`)
   )
 
-# I think we should check the data again as there might still be some abnormal values I've missed
+# Potentially problematic values: humidity values of 100
 
 # Pivot longer and visualize the daily data
 ddat |>
@@ -70,101 +71,8 @@ hdat |>
        y = "Value") +
   theme_minimal()
 
-# Plot daily streamflow data with horizontal line showing flood threshold
-ddat |> ggplot() +
-  geom_line(aes(y = `Streamflow Ave`, x = as.Date(Date))) +
-  geom_hline(aes(yintercept = 4.076)) +
-  ggtitle("Langrivier daily streamflow") +
-  xlab("Date") +
-  ylab("Streamflow (Cubic metres per second)")
+# save cleaned data
+write_csv(ddat, "data/data_daily_cleaned.csv")
+write_csv(hdat, "data/data_hourly_cleaned.csv")
 
-# Plot hourly streamflow data with horizontal line showing flood threshold
-hdat |> ggplot() +
-  geom_line(aes(y = `Streamflow`, x = as.Date(Date))) +
-  geom_hline(aes(yintercept = 4.076)) +
-  ggtitle("Langrivier hourly streamflow") +
-  xlab("Date") +
-  ylab("Streamflow (Cubic metres per second)")
-
-
-# # Assume you have a vector of streamflows called 'streamflow'
-# threshold <- 4.076 # Example threshold
-# exceedance_probability <- 1 - pnorm(threshold, mean(streamflow$value, na.rm = T), sd(streamflow$value, na.rm = T))
-# print(exceedance_probability)
-
-
-# Start with a null time-series model using JAGS
-# Load JAGS package
-library(rjags)
-
-# Define the model
-RandomWalk = "
-model{
-  
-  #### Data Model
-  for(t in 1:n){
-    y[t] ~ dnorm(x[t],tau_obs)
-  }
-  
-  #### Process Model
-  for(t in 2:n){
-    x[t]~dnorm(x[t-1],tau_add)
-  }
-  
-  #### Priors
-  x[1] ~ dnorm(x_ic,tau_ic) ## priors on the initial conditions (since this is unknown)
-  tau_obs ~ dgamma(a_obs,r_obs) ## prior on observation error
-  tau_add ~ dgamma(a_add,r_add) ## prior on process error
-}
-"
-
-# Format data for model
-time <- ddat$Date
-y <- ddat$`Streamflow Ave`
-data <- list(y= y,n=length(y),
-             x_ic=0.5,tau_ic=100, ## initial condition prior
-             a_obs=1,r_obs=1,           ## obs error prior
-             a_add=1,r_add=1)            ## process error prior
-
-nchain = 3
-init <- list()
-for(i in 1:nchain){
-  y.samp = sample(y,length(y),replace=TRUE)
-  init[[i]] <- list(tau_add=1/var(diff(y.samp)),  ## initial guess on process precision
-                    tau_obs=5/var(y.samp))        ## initial guess on obs precision
-}
-
-# Run the model
-j.model   <- jags.model (file = textConnection(RandomWalk),
-                         data = data,
-                         inits = init,
-                         n.chains = 3)
-
-# Sample from model 
-jags.out   <- coda.samples (model = j.model,
-                            variable.names = c("tau_add","tau_obs"),
-                            n.iter = 1000)
-# See if convergence has happened
-plot(jags.out)
-
-jags.out   <- coda.samples (model = j.model,
-                            variable.names = c("x","tau_add","tau_obs"),
-                            n.iter = 10000)
-
-burnin = 100                                   ## determine convergence
-jags.burn <- window(jags.out, start = burnin)  ## remove burn-in
-                             ## check diagnostics post burn-in
-
-time.rng = c(1,length(time))       ## adjust to zoom in and out
-out <- as.matrix(jags.out)         ## convert from coda to matrix  
-x.cols <- grep("^x",colnames(out)) ## grab all columns that start with the letter x
-ci <- apply(exp(out[,x.cols]),2,quantile,c(0.025,0.5,0.975)) ## model was fit on log scale
-
-plot(time,ci[2,],type='n',ylim=range(y,na.rm=TRUE),ylab="Streamflow",log='y',xlim=time[time.rng])
-## adjust x-axis label to be monthly if zoomed
-if(diff(time.rng) < 100){ 
-  axis.Date(1, at=seq(time[time.rng[1]],time[time.rng[2]],by='month'), format = "%Y-%m")
-}
-ecoforecastR::ciEnvelope(time,ci[1,],ci[3,],col=ecoforecastR::col.alpha("lightBlue",0.75))
-points(time,y,pch="+",cex=0.5)
 
