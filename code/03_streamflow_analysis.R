dc9134316d0df2b6270f0e86f885ff6402cbdb8d
Jessica Prevost <jessicaprevost@Jessicas-MacBook-Air-2.local>
2025-07-24 13:22:22 +0200

    Changed null model to log transform streamflow data

--- code/03_streamflow_analysis.R
+++ code/03_streamflow_analysis.R
@@ -37,7 +37,7 @@ cal_hdat <- hdat |> mutate(`Streamflow` = ifelse(Date < "2024-01-01", `Streamflo
 
 ## Start with a null time-series model using JAGS
 # Define the model
-RandomWalk = "
+RandomWalk <- "
 model{
   
   #### Data Model
@@ -45,13 +45,13 @@ model{
     y[t] ~ dnorm(x[t],tau_obs)
   }
   
-  #### Process Model
+  #### Process Model (random walk)
   for(t in 2:n){
-    x[t]~dnorm(x[t-1],tau_add)
+    x[t] ~ dnorm(x[t-1],tau_add)
   }
-  
+
   #### Priors
-  x[1] ~ dnorm(x_ic,tau_ic) ## priors on the initial conditions (since this is unknown)
+  x[1] ~ dnorm(x_ic, tau_ic)
   tau_obs ~ dgamma(a_obs,r_obs) ## prior on observation error
   tau_add ~ dgamma(a_add,r_add) ## prior on process error
 }
@@ -61,23 +61,25 @@ model{
 # Format data for model
 time <- cal_ddat$Date
 y <- cal_ddat$`Streamflow Ave`
-data <- list(y= y,n=length(y),
-             x_ic=0.5,tau_ic=100, ## initial condition prior
+z <- ddat$`Streamflow Ave` # for plotting later
+
+data <- list(y=log(y),n=length(y),      ## data
+             x_ic=log(0.1),tau_ic=0.1,    ## initial condition prior
              a_obs=1,r_obs=1,           ## obs error prior
-             a_add=1,r_add=1)            ## process error prior
+             a_add=1,r_add=1            ## process error prior
+)
 
 nchain = 3
-init <- list()
-for(i in 1:nchain){
-  y.samp = sample(y,length(y),replace=TRUE)
-  init[[i]] <- list(tau_add=1/var(diff(y.samp)),  ## initial guess on process precision
-                    tau_obs=5/var(y.samp))        ## initial guess on obs precision
-}
+# init <- list()
+# for(i in 1:nchain){
+#   y.samp = sample(y,length(y),replace=TRUE)
+#   init[[i]] <- list(tau_add=1/var(diff(log(y.samp))),  ## initial guess on process precision
+#                     tau_obs=5/var(log(y.samp)))        ## initial guess on obs precision
 
 # Run the model
 j.model   <- jags.model (file = textConnection(RandomWalk),
                          data = data,
-                         inits = init,
+                         # inits = init,
                          n.chains = 3)
 
 # Sample from model without x to check that convergence has happened
@@ -89,7 +91,7 @@ plot(jags.out)
 
 jags.out   <- coda.samples (model = j.model,
                             variable.names = c("x","tau_add","tau_obs"),
-                            n.iter = 5000)
+                            n.iter = 10000)
 
 burnin = 1000                                   ## determine convergence
 jags.burn <- window(jags.out, start = burnin)  ## remove burn-in
@@ -98,9 +100,9 @@ jags.burn <- window(jags.out, start = burnin)  ## remove burn-in
 time.rng = c(1,length(time))       ## adjust to zoom in and out
 out <- as.matrix(jags.out)         ## convert from coda to matrix  
 x.cols <- grep("^x",colnames(out)) ## grab all columns that start with the letter x
-ci <- apply(out[,x.cols],2,quantile,c(0.025,0.5,0.975)) ## model was fit on log scale
+ci <- apply(exp(out[,x.cols]),2,quantile,c(0.025,0.5,0.975)) ## model was fit on log scale
 
-plot(time,ci[2,],type='n',ylim=range(y,na.rm=TRUE),ylab="Streamflow average",xlim=time[time.rng])
+plot(time,ci[2,],type='n',ylim=range(y,na.rm=TRUE),ylab="Streamflow average", log='y', xlim=time[time.rng])
 ## adjust x-axis label to be monthly if zoomed
 if(diff(time.rng) < 100){ 
   axis.Date(1, at=seq(time[time.rng[1]],time[time.rng[2]],by='month'), format = "%Y-%m")
@@ -110,30 +112,9 @@ ecoforecastR::ciEnvelope(time,ci[1,],ci[3,],col=ecoforecastR::col.alpha("lightBl
 included <- !is.na(y)
 heldout <- is.na(y)
 # Plot included data points (model saw these)
-points(time[included], y[included], pch="+", col='black', cex=0.6)  # filled black dots
+points(time[included], z[included], pch="+", col='black', cex=0.6)  # filled black dots
 # Plot held-out data points (model did NOT see these)
-points(time[heldout], y[heldout], pch=1, col='red', cex=0.8)       # open red circles 
-
+points(time[heldout], z[heldout], pch=1, col='red', cex=0.8)       # open red circles 
 
 ## Now let's see if we can add covariates to the model
 # We will use the daily rainfall data as a covariate but the 
-# Define the model
-RandomWalk = "
-model{
-  
-  #### Data Model
-  for(t in 1:n){
-    y[t] ~ dnorm(x[t],tau_obs)
-  }
-  
-  #### Process Model
-  for(t in 2:n){
-    x[t]~dnorm(x[t-1],tau_add)
-  }
-  
-  #### Priors
-  x[1] ~ dnorm(x_ic,tau_ic) ## priors on the initial conditions (since this is unknown)
-  tau_obs ~ dgamma(a_obs,r_obs) ## prior on observation error
-  tau_add ~ dgamma(a_add,r_add) ## prior on process error
-}
-"
